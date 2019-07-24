828c3cc8e07e55cefe0d04993e5e27e920b3b355
sushree mishra <sushree022@gmail.com>
2019-07-24 14:46:35 +0530

    logic for sorting names added

--- src/main/java/com/stackroute/pe5/SortNames.java
+++ src/main/java/com/stackroute/pe5/SortNames.java
@@ -1,10 +1,21 @@
 package com.stackroute.pe5;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.TreeSet;
 
 public class SortNames {
     public ArrayList<String> sortingUnOrderedNames(String input)
     {
-        return null;
+        String[] inputArray = input.split(" ");
+        TreeSet<String> sortedResult= new TreeSet<String>();
+        ArrayList<String> result = new ArrayList<String>();
+
+        for(String i:inputArray) {
+
+            sortedResult.add(i);
+        }
+        result.addAll(sortedResult);
+        return result;
     }
 }
