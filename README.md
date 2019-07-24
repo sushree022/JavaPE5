277611eb4d070e194dcfab89f79df32ac50ddb53
sushree mishra <sushree022@gmail.com>
2019-07-24 10:57:10 +0530

    test case and logic for updating element in arraylist added

---
+++ README.md
@@ -0,0 +1,10 @@
+Author- Sushree Swagatika Mishra
+
+Project Name- Practice exercise 5
+
+Description- There are 6 problems for which test cases are written and then logic for those are written.
+
+1. ArrayListUpdate- 
+		In a given arraylist the elements of arraylist are replaced and cleared.
+
+
---
+++ src/test/java/com/stackroute/pe5/ArrayListUpdateTest.java
@@ -0,0 +1,66 @@
+package com.stackroute.pe5;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class ArrayListUpdateTest {
+    ArrayListUpdate arrayListUpdate;
+
+
+    /* This methods runs, before running any one of the test case .
+       It is used to initialize the required variables
+    */
+    @Before
+    public void setup() {
+//        arrange
+        arrayListUpdate = new ArrayListUpdate();
+    }
+
+    /* This method runs, after running all the test cases
+       It is used to clear the initialized variables
+    */
+    @After
+    public void teardown() {
+        arrayListUpdate = null;
+    }
+
+    @Test
+    public void givenStringShouldReplaceArrayElementOfAnArrayList()
+    {
+//        act
+        ArrayList<String> fruitList= new ArrayList<String>();
+        fruitList.add("Apple");
+        fruitList.add("Grape");
+        fruitList.add("Melon");
+        fruitList.add("Berry");
+        ArrayList<String> expectedResult= new ArrayList<String>();
+        expectedResult.add("Kiwi");
+        expectedResult.add("Grape");
+        expectedResult.add("Melon");
+        expectedResult.add("Berry");
+//        assert
+        ArrayList actualResult=arrayListUpdate.updateArrayList(fruitList,"Apple", "Kiwi");
+        assertEquals(expectedResult,actualResult);
+    }
+
+    
+    @Test
+    public void givenArrayListShouldReturnEmptyArrayList()
+    {
+        //        act
+        ArrayList<String> fruitList= new ArrayList<String>();
+        fruitList.add("Apple");
+        fruitList.add("Grape");
+        fruitList.add("Melon");
+        fruitList.add("Berry");
+        ArrayList<String> expectedResult= new ArrayList<String>();
+//        assert
+        ArrayList actualResult=arrayListUpdate.clearArrayList(fruitList);
+        assertEquals(expectedResult,actualResult);
+    }
+}
